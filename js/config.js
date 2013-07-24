d3702e7553373bdd61b49808ec1a3dfdaafb52d9
Ossi Hanhinen <hanhinen@gmail.com>
2013-07-24 09:07:36 -0400

    deployment ready

---
+++ js/config.js
@@ -0,0 +1,30 @@
+/* 
+  Aalto Journey Planner configuration
+  
+  Copy this file as config.js and insert the correct configuration
+  
+  Following optional parameters are available for location items:
+  
+  "nomap": no marker on the map
+  "start": default start point
+  "end": default end poit
+  
+  Reittiopas account can be requested from:
+  http://developer.reittiopas.fi/pages/en/account-request.php
+ */
+
+var config = {
+  "api":"apiProxy/",
+  "user":"aaltowindow",
+  "pass":"multiTouch",
+  "routeRefreshMinTime":2000, // milliseconds
+  "longPressTime":500, // milliseconds
+  "longPressThreshold":10, //pixels
+  "locs": {
+    "mapcenter": {
+      "lat":60.181925,
+      "lng":24.902101,
+      "nomap":true
+    }
+  }
+}
